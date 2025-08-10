118162d7fc95f5a36bd26b8f0165c5fa9b7e8c34
Wanoski Martinez <wanoski.martinez@gmail.com>
2025-08-10 07:43:34 +0000

    devine quoi

--- next.config.ts
+++ next.config.ts
@@ -4,6 +4,7 @@ import type {NextConfig} from 'next';
 const nextConfig: NextConfig = {
   output: 'export',
   basePath: '/LightAudio',
+  assetPrefix: '/LightAudio/',
   typescript: {
     ignoreBuildErrors: true,
   },
