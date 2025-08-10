385edddc83e7ed0ef5c6ae52cb16f5b71203983a
Wanoski Martinez <wanoski.martinez@gmail.com>
2025-08-10 05:59:09 +0000

    comment publier ceci sur github pages ? actuellement, j'ai ce message

--- next.config.ts
+++ next.config.ts
@@ -34,6 +34,7 @@ const withPWA = require('next-pwa')({
 })
 
 const nextConfig: NextConfig = {
+  output: 'export',
   /* config options here */
   typescript: {
     ignoreBuildErrors: true,
@@ -42,6 +43,7 @@ const nextConfig: NextConfig = {
     ignoreDuringBuilds: true,
   },
   images: {
+    unoptimized: true,
     remotePatterns: [
       {
         protocol: 'https',
