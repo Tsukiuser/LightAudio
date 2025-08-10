47b3b04c9dc84b5e4399326afa1bdf96e2e58ec2
Wanoski Martinez <wanoski.martinez@gmail.com>
2025-08-10 07:18:21 +0000

    et dcp on ne pourra plus utiliser pwa ?

--- next.config.ts
+++ next.config.ts
@@ -5,7 +5,7 @@ const withPWA = require('next-pwa')({
   dest: 'public',
   register: true,
   skipWaiting: true,
-  disable: process.env.NODE_ENV === 'development' ? false : true,
+  disable: process.env.NODE_ENV === 'development',
   runtimeCaching: [
     // Cache Google Fonts
     {
